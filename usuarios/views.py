3300c92adb40271a3a3dcdef547c084754df2d20
Maria Carolina <101002560+Carolmesqu@users.noreply.github.com>
2023-10-04 22:32:36 -0300

    DEV: Finalizando Login e criando Exames configurações iniciais

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.messages import constants
 from django.contrib import messages
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login
 
 
 def validar_nome_de_usuario(username):
@@ -69,17 +69,19 @@ def cadastro(request):
         return redirect("/usuarios/cadastro")
 
 
-def login(request):
+def logar(request):
     if request.method == "GET":
         return render(request, "login.html")
     elif request.method == "POST":
         username = request.POST.get("username")
         senha = request.POST.get("senha")
         user = authenticate(username=username, password=senha)
-    # if user:
-    #     login(request, user)
-    #     # Acontecerá um erro ao redirecionar por enquanto, resolveremos nos próximos passos
-    #     return redirect('/')
-    # elif:
-    #     messages.add_message(request, constants.ERROR, 'Usuario ou senha inválidos')
-    #     return redirect('/usuarios/login')
+    if user:
+        login(request, user)
+        # Acontecerá um erro ao redirecionar por enquanto, resolveremos nos próximos passos
+        return redirect('/')
+    else:
+        messages.add_message(request, constants.ERROR, 'Usuario ou senha inválidos')
+        return redirect('/usuarios/login')
+
+
