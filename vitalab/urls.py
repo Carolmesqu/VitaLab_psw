6fea15d9682f98d65648ce35ae238e002b1ca9be
Maria Carolina <101002560+Carolmesqu@users.noreply.github.com>
2023-10-07 20:00:11 -0300

    DEV: Gerar acesso médico (finalizado), Gerar acesso médico (finalizado), Gerenciar exames dos clientes (finalizado) e Gerar senha (em andamento)

--- vitalab/urls.py
+++ vitalab/urls.py
@@ -4,7 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('usuarios/', include('usuarios.urls')),
-    path('exames/', include('exames.urls')),
+    path("admin/", admin.site.urls),
+    path("usuarios/", include("usuarios.urls")),
+    path("exames/", include("exames.urls")),
+    path("empresarial/", include("empresarial.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
